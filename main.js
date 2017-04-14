9418456eb18e37d0873b92c6fc73d6a607193a9c
Levi Donaldson <lcdbacanas@gmail.com>
2017-04-14 02:02:00 -0500

    popup working

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-  $(window).load(function() {
-    console.log('asdfs')
+  function overlay () {
+    $('.overlay').removeClass('hide');
+  }
+
+  function openPopup() {
     $('#popup-wrapper').addClass('slide_up');
-  })
-})
+  }
+
+  $( window ).on( "load", function() {
+    setTimeout(overlay, 1000);
+    setTimeout(openPopup, 2000);
+  });
+
+});
