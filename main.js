6dda650635fac7a1a908fb96dc648fa8c9b1e46a
Levi Donaldson <lcdbacanas@gmail.com>
2017-04-28 23:50:43 -0500

    fixing exit

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-  
+
   function overlay () {
     $('.overlay').removeClass('hide');
-  };
+  }
 
   function openPopup() {
     $('#popup-wrapper').addClass('slide_up');
-  };
-  
+  }
+
   function exit() {
-    document.getElementById("popup-wrapper")this.setAttribute("class", "remove");
-  };
-  
+    document.getElementById("popup-wrapper");this.setAttribute("class", "remove");
+  }
+
   $( window ).on( "load", function() {
     console.log('popup works');
     setTimeout(overlay, 1000);
     setTimeout(openPopup, 2000);
-    
+
   });
 
 });
