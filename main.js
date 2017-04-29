8d110abb460f709da8785f5f1a6561dc39942e13
Levi Donaldson <lcdbacanas@gmail.com>
2017-04-29 00:03:23 -0500

    exit error fixed

--- main.js
+++ main.js
@@ -7,24 +7,15 @@ $(document).ready(function () {
   function openPopup() {
     $('#popup-wrapper').addClass('slide_up');
   }
-<<<<<<< HEAD
 
-  function exit() {
-    document.getElementById("popup-wrapper");this.setAttribute("class", "remove");
-  }
+  $('#exit').on('click', function () {
+    $('#popup-wrapper').hide();
+  });
 
-=======
-  
-  function exit() {
-    document.getElementById("popup-wrapper");this.setAttribute("class", "remove");
-  }
-  
->>>>>>> 1fd1af52d542f102322c31a46281c475aad19f12
   $( window ).on( "load", function() {
     console.log('popup works');
     setTimeout(overlay, 1000);
     setTimeout(openPopup, 2000);
-
   });
 
 });
