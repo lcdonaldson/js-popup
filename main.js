cf4acc9c18e279db82b77474ee81006adaa11991
Levi Donaldson <lcdbacanas@gmail.com>
2017-04-28 22:22:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,22 +2,22 @@ $(document).ready(function () {
   
   function overlay () {
     $('.overlay').removeClass('hide');
-  }
+  };
 
   function openPopup() {
     $('#popup-wrapper').addClass('slide_up');
-  }
+  };
+  
+  $('#exit').on("click", function () {
+    console.log('works');
+    $('#popup-wrapper').addClass('hide');
+  });
   
   $( window ).on( "load", function() {
     console.log('popup works');
     setTimeout(overlay, 1000);
     setTimeout(openPopup, 2000);
     
-  });
-  
-  $('#exit').on("click", function () {
-    console.log('works');
-    $('#popup-wrapper').addClass('hide');
   });
 
 });
